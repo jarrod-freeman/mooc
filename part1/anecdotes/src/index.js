7a674d65b5c685a4de1eef39cbc3e274296ed38f
Jarrod Freeman <51888879+jarrod-freeman@users.noreply.github.com>
2019-07-07 23:40:15 -0400

    exercise 1.13

--- part1/anecdotes/src/index.js
+++ part1/anecdotes/src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 
 const App = (props) => {
     const [selected, setSelected] = useState(0);
+    const [votes, setVotes] = useState(new Array(props.anecdotes.length).fill(0))
 
     const getNext = () => {
         let random = Math.floor(Math.random() * 6);
@@ -11,13 +12,24 @@ const App = (props) => {
         setSelected(random);
     }
 
+    const handleVote = () => {
+        let copy = new Array(...votes);
+        copy[selected]++;
+
+        setVotes(copy)
+    }
+
     return (
         <div>
-            <p>{props.anecdotes[selected]}</p>
+            <div>
+                {props.anecdotes[selected]}
+                <div>has {votes[selected]} votes</div>
+            </div>
+            <button onClick={handleVote}>vote</button>
             <button onClick={getNext}>next anecdote</button>
         </div>
     )
-}
+};
 
 const anecdotes = [
     'If it hurts, do it more often',
@@ -25,7 +37,7 @@ const anecdotes = [
     'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
     'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
     'Premature optimization is the root of all evil.',
-    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
-  ]
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+  ];
 
 ReactDOM.render(<App anecdotes={anecdotes} />, document.getElementById('root'));
