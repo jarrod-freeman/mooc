7b06536716bbb5b07102b8999d0345667f6a9211
Jarrod Freeman <51888879+jarrod-freeman@users.noreply.github.com>
2019-07-09 01:30:11 -0400

    exercise 2.9

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -3,16 +3,19 @@ import Person from './components/Person';
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { 
-      name: 'Arto Hellas',
-      number: '040-1234567'
-    }
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
   ]);
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('');
+  const [filter, setFilter] = useState('');
 
   const personRows = () => {
-    return persons.map(person => {
+    return persons.filter(person => {
+      return person.name.toLowerCase().indexOf(filter.toLowerCase()) > -1
+    }).map(person => {
       return (
         <Person key={person.name} person={person} />
       )
@@ -44,17 +47,23 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
-      <form onSubmit={submitNewName}>
-        <div>
-          name: <input value={newName} onChange={(e) => setNewName(e.target.value)} />
-        </div>
-        <div>
-          number: <input value={newNumber} onChange={(e) => setNewNumber(e.target.value)} />
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
+      <div>
+        filter shown with <input value={filter} onChange={(e) => setFilter(e.target.value)} />
+      </div>
+      <h2>add a new</h2>
+      <div>
+        <form onSubmit={submitNewName}>
+          <div>
+            name: <input value={newName} onChange={(e) => setNewName(e.target.value)} />
+          </div>
+          <div>
+            number: <input value={newNumber} onChange={(e) => setNewNumber(e.target.value)} />
+          </div>
+          <div>
+            <button type="submit">add</button>
+          </div>
+        </form>
+      </div>
       <h2>Numbers</h2>
       {personRows()}
     </div>
