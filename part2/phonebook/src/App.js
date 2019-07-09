5bd5a542c420103997521c1669d44302fe557f02
Jarrod Freeman <51888879+jarrod-freeman@users.noreply.github.com>
2019-07-09 00:25:56 -0400

    exercise 2.6

---
+++ part2/phonebook/src/App.js
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+import Person from './components/Person';
+
+const App = () => {
+  const [ persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]);
+  const [ newName, setNewName ] = useState('');
+
+  const personRows = () => persons.map(person => <Person key={person.name} person={person} />);
+
+  const submitNewName = (event) => {
+    event.preventDefault();
+    
+    let person = {
+      name: newName
+    };
+
+    setPersons(persons.concat(person));
+    setNewName('');
+  };
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={submitNewName}>
+        <div>
+          name: <input value={newName} onChange={(e) => setNewName(e.target.value)} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {personRows()}
+    </div>
+  );
+};
+
+export default App
