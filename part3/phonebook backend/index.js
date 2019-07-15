dea1298b6569df56e132dcebb84d27ef6df723a8
Jarrod Freeman <51888879+jarrod-freeman@users.noreply.github.com>
2019-07-14 23:54:22 -0400

    exercise 3.3

--- part3/phonebook backend/index.js
+++ part3/phonebook backend/index.js
@@ -33,6 +33,18 @@ app.get('/api/persons', (request, response) => {
     response.json(people);
 });
 
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id);
+    const person = people.find(person => person.id === id);
+
+    if(person){
+        response.json(person);
+    }
+    else{
+        response.status(404).end();
+    }
+});
+
 const port = 3001;
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
