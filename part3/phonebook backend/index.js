f256ed2239d6f234e88ed33bc620325a60ae794e
Jarrod Freeman <51888879+jarrod-freeman@users.noreply.github.com>
2019-07-30 00:56:10 -0400

    exercise 3.13

--- part3/phonebook backend/index.js
+++ part3/phonebook backend/index.js
@@ -20,29 +20,6 @@ app.use(cors());
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
 app.use(express.static('build'));
 
-let persons = [
-    {
-        "name": "Arto Hellas",
-        "number": "040-123456",
-        "id": 1
-      },
-      {
-        "name": "Ada Lovelace",
-        "number": "39-44-5323523",
-        "id": 2
-      },
-      {
-        "name": "Dan Abramov",
-        "number": "12-43-234345",
-        "id": 3
-      },
-      {
-        "name": "Mary Poppendieck",
-        "number": "39-23-6423122",
-        "id": 4
-      }
-];
-
 app.get('/info', (request, response) => {
     response.send(`<div>Phonebook has info for ${people.length} people</div><p>${new Date()}</p>`);
 });
@@ -74,29 +51,28 @@ app.post('/api/persons', (request, response) => {
         });
     }
 
-    if(persons.find(person => person.name === body.name)){
-        return response.status(400).json({
-            error: 'name must be unique'
-        });
-    }
+    // if(persons.find(person => person.name === body.name)){
+    //     return response.status(400).json({
+    //         error: 'name must be unique'
+    //     });
+    // }
 
-    let person ={
-        id: Math.floor(Math.random() * 100000),
+    let person = new Person({
         name: body.name,
         number: body.number
-    };
+    });
 
-    persons = persons.concat(person);
-
-    response.json(person);
+    person.save().then(savedPerson => {
+        response.json(savedPerson.toJSON());
+    })
 });
 
 app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id);
+    // const id = Number(request.params.id);
 
-    persons = persons.filter(person => person.id !== id);
+    // persons = persons.filter(person => person.id !== id);
 
-    response.status(204).end();
+    // response.status(204).end();
 });
 
 const PORT = process.env.PORT;
