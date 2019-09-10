b747589da550fcb55e77c5a2de527b980d2f45de
Jarrod Freeman <51888879+jarrod-freeman@users.noreply.github.com>
2019-09-09 20:34:00 -0400

    exercise 5.6

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,8 +1,40 @@
-import React from 'react'
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+import React, { useState } from 'react';
+
+const Blog = ({ blog }) => {
+    const [detailsVisible, setDetailsVisible] = useState(false);
+
+    const showWhenVisible = { display: detailsVisible ? '' : 'none' };
+
+    const blogStyle = {
+        paddingTop: 10,
+        paddingLeft: 2,
+        border: 'solid',
+        borderWidth: 1,
+        marginBottom: 5
+    };
+
+    const handleBlogClick = (e) => {
+        setDetailsVisible(!detailsVisible);
+    };
+
+    return (
+        <div style={blogStyle}>
+            <div onClick={handleBlogClick}>
+                {blog.title}
+            </div>
+            <div style={showWhenVisible}>
+                <div>
+                    {blog.url}
+                </div>
+                <div>
+                    {blog.likes} likes <button>like</button>
+                </div>
+                <div>
+                    added by {blog.author}
+                </div>
+            </div>
+        </div>
+    );
+};
 
 export default Blog
