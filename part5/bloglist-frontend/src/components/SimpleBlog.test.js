262b10db35aa2857e1182d069c57550164b090c7
Jarrod Freeman <51888879+jarrod-freeman@users.noreply.github.com>
2019-09-10 22:07:00 -0400

    exercise 5.14

--- part5/bloglist-frontend/src/components/SimpleBlog.test.js
+++ part5/bloglist-frontend/src/components/SimpleBlog.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import SimpleBlog from './SimpleBlog';
 
 describe('simple blog component', () => {
@@ -25,5 +25,22 @@ describe('simple blog component', () => {
         const component = render(<SimpleBlog blog={blog} />);
         const div = component.container.querySelector('.blogLikes');
         expect(div).toHaveTextContent('blog has 5 likes');
+    });
+
+    test('the event handler is called when the button is clicked', () => {
+        const mockClickHandler = jest.fn();
+        const blog = {
+            title: 'Blog Title',
+            author: 'Blog Author',
+            likes: 5
+        };
+
+        const component = render(<SimpleBlog blog={blog} onClick={mockClickHandler} />);
+        const button = component.container.querySelector('button');
+
+        fireEvent.click(button);
+        fireEvent.click(button);
+
+        expect(mockClickHandler.mock.calls.length).toBe(2);
     });
 });
